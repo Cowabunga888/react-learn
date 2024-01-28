706245f200a6d2e996d03b8a56b8a42ec467112d
minhhieu2122000@gmail.com <minhhieu2122000@gmail.com>
2024-01-28 23:10:24 +0700

    useEffect part 01

--- src/pages/hooks-pages/useEffect/index.tsx
+++ src/pages/hooks-pages/useEffect/index.tsx
@@ -1,15 +1,58 @@
 import React, { useEffect, useState } from 'react'
+import AppBlockQuote from '../../../components/block-quotes'
 
 function UseEffectPage() {
-  const [value, setValue] = useState<string>('')
+  const [data, setData] = useState<any>([])
 
-  useEffect(()=>{
-    console.log('this line log when input change value')
+  const handleFetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(res => setData(res))
+  }
+
+  useEffect(() => {
+    // Mouted is in this block
+    console.log('Mouted')
   })
+
+  useEffect(() => {
+    // chỉ gọi 1 lần sau khi component đã MOUTED
+    // tránh trường hợp call back setstate liên tục làm cho component render vô tận
+    handleFetchData()
+  }, [])
 
 
   return (
-    <input type="text" onChange={(e)=>setValue(e.target.value)}/>
+    // Render is in this block
+    <>
+      {data?.map((user: any) => {
+        return (
+          <p key={user?.id}>{user?.id}. {user?.name}</p>
+        )
+      })}
+
+      {console.log('Render')}
+
+      <AppBlockQuote>
+        <p>1. useEffect(callback)</p>
+        <p>2. useEffect(callback, [])</p>
+        <p>3. useEffect(callback, [deps])</p>
+        <p>===================================</p>
+        <p>## Call back trong useEffect luôn luôn được gọi sau khi component mouted</p>
+        <p>## Tương ứng 3 case trên thì:</p>
+        <p>1. Callback luôn được gọi sau khi component mouted hoặc re-render</p>
+        <p>2. Callback được gọi duy nhất 1 lần sau khi compoent mouted</p>
+        <p>3. Callback chỉ được gọi khi có sự thay đổi của deps</p>
+        <p>===================================</p>
+        <p>## Render luôn chạy trước khi component mounted</p>
+        <p>## Render nằm trong phần return của component</p>
+        <p>## Component Mounted - sau khi component render và chạy trong useEffect</p>
+        <p>===================================</p>
+        <p>## Khái niệm render chỉ về DOM</p>
+        <p>## Khái niệm mouted chỉ về trạng thái của component sau khi render</p>
+      </AppBlockQuote>
+
+    </>
   )
 }
 
@@ -18,9 +61,18 @@ function UseEffectPage() {
   2. useEffect(callback, [])
   3. useEffect(callback, [deps])
   ===================================
-  1. callback luôn được gọi sau khi component mouted hoặc re-render
-  2. callback được gọi duy nhất 1 lần sau khi compoent mouted
-  3. callback chỉ được gọi khi có sự thay đổi của deps
+  ## Call back trong useEffect luôn luôn được gọi sau khi component mouted
+  ## Tương ứng 3 case trên thì:
+  1. Callback luôn được gọi sau khi component mouted hoặc re-render
+  2. Callback được gọi duy nhất 1 lần sau khi compoent mouted
+  3. Callback chỉ được gọi khi có sự thay đổi của deps
+  ===================================
+  ## Render luôn chạy trước khi component mounted
+  ## Render nằm trong phần return của component
+  ## Component Mounted - sau khi component render và chạy trong useEffect
+  ===================================
+  ## Khái niệm render chỉ về DOM
+  ## Khái niệm mouted chỉ về trạng thái của component sau khi render
 */
 
 export default UseEffectPage
