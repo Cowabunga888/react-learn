15b0655ad083bb5a6188ed3dd239c8d510f3499b
minhhieu2122000@gmail.com <minhhieu2122000@gmail.com>
2024-01-29 20:21:37 +0700

    useEffect part 03

--- src/pages/hooks-pages/useEffect/useEffect-general.tsx
+++ src/pages/hooks-pages/useEffect/useEffect-general.tsx
@@ -2,54 +2,28 @@ import { useEffect, useState } from 'react'
 import AppBlockQuote from '../../../components/block-quotes'
 
 function UseEffectGeneral() {
-  const [data, setData] = useState<any>([])
-
-  const handleFetchData = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(res => setData(res))
-  }
-
-  useEffect(() => {
-    // Mouted is in this block
-    console.log('Mouted')
-  })
-
-  useEffect(() => {
-    // chỉ gọi 1 lần sau khi component đã MOUTED
-    // tránh trường hợp call back setstate liên tục làm cho component render vô tận
-    handleFetchData()
-  }, [])
-
 
   return (
-    // Render is in this block
     <>
-      {data?.map((user: any) => {
-        return (
-          <p key={user?.id}>{user?.id}. {user?.name}</p>
-        )
-      })}
-
-      {console.log('Render')}
-
       <AppBlockQuote>
         <p>1. useEffect(callback)</p>
         <p>2. useEffect(callback, [])</p>
         <p>3. useEffect(callback, [deps])</p>
         <p>===================================</p>
-        <p className='text-red-400'>## Call back trong useEffect luôn luôn được gọi sau khi component mouted</p>
+        <p className='text-red-400'>## Call back trong useEffect luôn luôn được gọi sau khi component mounted</p>
         <p>## Tương ứng 3 case trên thì:</p>
         <p>1. Callback được gọi mỗi khi component re-render -- ít khi dùng</p>
-        <p>2. Callback được gọi duy nhất 1 lần sau khi component mouted</p>
+        <p>2. Callback được gọi duy nhất 1 lần sau khi component mounted</p>
         <p>3. Callback chỉ được gọi khi có sự thay đổi của deps</p>
         <p>===================================</p>
         <p>## Render luôn chạy trước khi component mounted</p>
         <p>## Render nằm trong phần return của component</p>
         <p>## Component Mounted - sau khi component render và chạy trong useEffect</p>
         <p>===================================</p>
-        <p>## Khái niệm render chỉ về DOM</p>
-        <p>## Khái niệm mouted === đã render xong</p>
+        <p>## Khái niệm render chỉ về DOM và là 1 trong 3 trạng thái của Mounting</p>
+        <p>## Một component thực hiện "mount" chỉ khi nó render trong lần đầu tiên. Có 3 mounting lifecycle methods:</p>
+        <p>## componentWillMount || render || componentDidMount</p>
+        <p>## Khái niệm mount === componentDidMount</p>
       </AppBlockQuote>
 
     </>
@@ -71,7 +45,9 @@ function UseEffectGeneral() {
   ## Render nằm trong phần return của component
   ## Component Mounted - sau khi component render và chạy trong useEffect
   ===================================
-  ## Khái niệm render chỉ về DOM
-  ## Khái niệm mouted === đã render xong
+  ## Khái niệm render chỉ về DOM và là 1 trong 3 trạng thái của Mounting
+  ## Một component thực hiện "mount" chỉ khi nó render trong lần đầu tiên. Có 3 mounting lifecycle methods:
+  ## ComponentWillMount || render || componentDidMount
+  ## Khái niệm mount === componentDidMount
 */
 export default UseEffectGeneral
