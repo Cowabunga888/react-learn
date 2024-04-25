b780d062e1fe36b47ef7e652d9760884cf6e136d
BuiMinhHieu <hieu.bm@softworld.jp>
2024-04-25 15:41:37 +0700

    about page: skills scrolling

---
+++ src/views/about/components/about-header/index.tsx
@@ -0,0 +1,9 @@
+function AboutHeader() {
+	return (
+		<div className="w-full h-screen blur-[2px]">
+			<video className="about-video-header" autoPlay loop src="/assets/videos/pages/about/intro.mp4" />
+		</div>
+	)
+}
+
+export default AboutHeader
--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import AboutHeader from './components/about-header'
+import ScrollingSkills from './components/scrolling-skills'
+import './css/style.css'
 
 function AboutPageView() {
-	return <div>AboutPageView</div>
+	return (
+		<div className="about-page">
+			<section className="about-section">
+				<AboutHeader />
+			</section>
+			<section className="about-section">
+				<ScrollingSkills />
+			</section>
+		</div>
+	)
 }
 
 export default AboutPageView
