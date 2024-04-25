95fea228deced8ec5941330a57cd9885b193da2d
BuiMinhHieu <hieu.bm@softworld.jp>
2024-04-25 17:12:36 +0700

    about page: skills scrolling

--- src/views/about/components/about-header/index.tsx
+++ src/views/about/components/about-header/index.tsx
@@ -1,9 +1,7 @@
+import './style.css'
+
 function AboutHeader() {
-	return (
-		<div className="w-full h-screen blur-[2px] overflow-hidden">
-			{/* <video className="about-video-header" autoPlay loop src="/assets/videos/pages/about/intro.mp4" /> */}
-		</div>
-	)
+	return <div className="w-full h-screen blur-[2px] overflow-hidden about-header"></div>
 }
 
 export default AboutHeader
