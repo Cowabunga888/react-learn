1fa1c810e88b553a9aff4d890c939bd0734a331d
BuiMinhHieu <hieu.bm@softworld.jp>
2024-04-25 16:18:09 +0700

    about page: skills scrolling

--- src/views/about/components/about-header/index.tsx
+++ src/views/about/components/about-header/index.tsx
@@ -1,7 +1,7 @@
 function AboutHeader() {
 	return (
-		<div className="w-full h-screen blur-[2px]">
-			<video className="about-video-header" autoPlay loop src="/assets/videos/pages/about/intro.mp4" />
+		<div className="w-full h-screen blur-[2px] overflow-hidden">
+			{/* <video className="about-video-header" autoPlay loop src="/assets/videos/pages/about/intro.mp4" /> */}
 		</div>
 	)
 }
